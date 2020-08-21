1c5efe7835320c09f39a8096c9454eeda9d24af2
Irina Uzkikh <uzkikh-i@yandex.ru>
2020-08-21 15:01:12 +0300

    with google form

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,8 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackMd5Hash = require('webpack-md5-hash');
-const MiniCssExtractPlugin = require("mini-css-extract-plugin"); // добавили плагин
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 module.exports = {
   entry: { 
       index: './src/index.js' },
@@ -21,13 +22,35 @@ module.exports = {
       {
         test: /\.css$/,
         use:  [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader'] // добавили минификацию CSS
-      }
+      },
+      {
+        test: /\.(png|jpg|gif|ico|svg)$/,
+        use: [
+                'file-loader?name=./images/[name].[ext]', // указали папку, куда складывать изображения
+                {
+                        loader: 'image-webpack-loader',
+                        options: {}
+                },
+        ]
+    },
+    {
+      test: /\.(eot|ttf|woff|woff2)$/,
+      loader: 'file-loader?name=./vendor/[name].[ext]'
+}
     ]
   },
   plugins: [
     new MiniCssExtractPlugin({ // 
       filename: 'style.[contenthash].css',
     }),
+    new OptimizeCssAssetsPlugin({
+        assetNameRegExp: /\.css$/g,
+        cssProcessor: require('cssnano'),
+        cssProcessorPluginOptions: {
+                preset: ['default'],
+        },
+        canPrint: true
+   }),
     new HtmlWebpackPlugin({
       inject: false,
       template: './src/index.html',
